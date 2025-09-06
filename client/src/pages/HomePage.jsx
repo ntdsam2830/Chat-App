6ea21210c4730c4832a561f1927fcce073533fdc
ntdsam2830 <ntdsam@gmail.com>
2025-09-06 12:22:25 +0700

    create home page

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const HomePage = () => {
   return (
-    <div>Home Page</div>
-  )
-}
+    <div className="border w-full h-screen sm:px-[15%] sm:py-[5%]">
+      Home Page
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
