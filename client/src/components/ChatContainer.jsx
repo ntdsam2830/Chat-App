c5482ff6b93db0eb706e9c60a4139008d411b3fe
ntdsam2830 <ntdsam@gmail.com>
2025-09-17 00:44:29 +0700

    add message to chat container

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import assets from "../assets/assets";
+import assets, { messagesDummyData } from "../assets/assets";
 
 const ChatContainer = ({ selectedUser, setSelectedUser }) => {
-  return (
+  return selectedUser ? (
     <div className="h-full overflow-scroll relative backdrop-blur-lg">
+      {/* Chat Header */}
       <div className="flex items-center gap-3 py-3 mx-4 border-b border-stone-500">
         <img src={assets.profile_martin} alt="" className="w-8 rounded-full" />
         <p className="flex-1 text-lg text-white flex items-center gap-2">
@@ -19,6 +20,42 @@ const ChatContainer = ({ selectedUser, setSelectedUser }) => {
         />
         <img src={assets.help_icon} alt="" className="md:hidden max-w-5" />
       </div>
+      {/* Chat Messages */}
+      <div className="flex flex-col h-[calc(100%-120px)] overflow-y-scroll p-3 pb-6">
+        {messagesDummyData.map((msg, index) => (
+          <div
+            key={index}
+            className={`flex items-end gap-2 justify-end ${
+              msg.senderId !== "680f50e4f10f3cd28382ecf9" && "flex-row-reverse"
+            }`}
+          >
+            {msg.image ? (
+              <img
+                src={msg.image}
+                alt=""
+                className="max-w-[230px] rounded-lg border border-gray-700 overflow-hidden mb-8"
+              />
+            ) : (
+              <p
+                className={`p-2 max-w-[200px] md:text-sm font-light rounded-lg mb-8 break-all bg-violet-500/30 text-white ${msg.senderId ==="680f50e4f10f3cd28382ecf9" ?
+                  "rounded-br-none"
+                  : "rounded-bl-none bg-[#3a3a5c]/50"
+                }`}
+              >
+                {msg.text}
+              </p>
+            )}
+            <div>
+              
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  ) : (
+    <div className="flex flex-col justify-center items-center gap-2 text-gray-500 bg-white/10 max-md:hidden">
+      <img src={assets.logo_icon} alt="" className="max-w-16" />
+      <p className="text-lg font-medium text-white">Chat anytime, anywhere</p>
     </div>
   );
 };
