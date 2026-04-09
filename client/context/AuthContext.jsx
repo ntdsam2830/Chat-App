02381fe560e034ebe2ff96c7beac486b1e65e572
ntdsam2830 <ntdsam@gmail.com>
2026-04-09 23:18:43 +0700

    feat(auth): enhance authentication flow with login, logout, and profile update functions

--- client/context/AuthContext.jsx
+++ client/context/AuthContext.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-refresh/only-export-components */
 import axios from "axios";
 import { createContext, useEffect, useState } from "react";
 import toast from "react-hot-toast";
@@ -28,6 +29,48 @@ export const AuthProvider = ({ children }) => {
     }
   };
 
+  const login = async (state, credentials) => {
+    try {
+      const { data } = await axios.post(`/api/auth/${state}`, credentials);
+      if (data.success) {
+        setToken(data.token);
+        localStorage.setItem("token", data.token);
+        setAuthUser(data.userData);
+        connectSocket(data.userData);
+        axios.defaults.headers.common["token"] = data.token;
+        toast.success(data.message);
+      } else {
+        toast.error(data.message || "Login failed");
+      }
+    } catch (error) {
+      toast.error(error.message || "Error logging in");
+    }
+  };
+
+  const logout = async () => {
+    localStorage.removeItem("token");
+    setToken(null);
+    setAuthUser(null);
+    setOnlineUsers([]);
+    axios.defaults.headers.common["token"] = null;
+    toast.success("Logged out successfully");
+    socket?.disconnect();
+  };
+
+  const updateProfile = async (body) => {
+    try {
+      const { data } = await axios.put("/api/auth/update-profile", body);
+      if (data.success) {
+        setAuthUser(data.user);
+        toast.success(data.message);
+      } else {
+        toast.error(data.message || "Failed to update profile");
+      }
+    } catch (error) {
+      toast.error(error.message || "Error updating profile");
+    }
+  };
+
   //Connect socket to handle real-time features like online users and messaging
   const connectSocket = (userData) => {
     if (!userData || socket?.connected) return;
@@ -47,7 +90,7 @@ export const AuthProvider = ({ children }) => {
       axios.defaults.headers.common["token"] = token;
     }
     checkAuth();
-  }, []);
+  });
 
   const value = {
     axios,
@@ -55,6 +98,9 @@ export const AuthProvider = ({ children }) => {
     authUser,
     onlineUsers,
     socket,
+    login,
+    logout,
+    updateProfile,
   };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
