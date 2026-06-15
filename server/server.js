1507869494ab107b6ed01ee97fe665b58372a17c
ntdsam2830 <ntdsam@gmail.com>
2026-06-15 23:29:58 +0700

    feat(app): update background image path and remove old SVG asset

--- server/server.js
+++ server/server.js
@@ -52,7 +52,11 @@ app.use("/api/messages", messageRouter);
 await connectDB();
 
 //Start the server
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
-  console.log(`Server is running on PORT: ${PORT}`);
-});
+if (process.env.NODE_ENV === "production") {
+  const PORT = process.env.PORT || 5000;
+  server.listen(PORT, () => {
+    console.log(`Server is running on PORT: ${PORT}`);
+  });
+}
+
+export default server;
