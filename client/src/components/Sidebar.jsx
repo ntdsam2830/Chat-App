c2f3ae130949129372dae422300d827f19e906c8
ntdsam2830 <ntdsam@gmail.com>
2026-06-15 21:54:02 +0700

    feat(chat): update online user indicator and limit unseen message display

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -105,7 +105,7 @@ const Sidebar = () => {
             </div>
             {unseenMessages[user._id] > 0 && (
               <p className="absolute top-4 right-4 text-xs h-5 w-5 flex justify-center items-center rounded-full bg-violet-500/50">
-                {unseenMessages[user._id]}
+                {unseenMessages[user._id] > 9 ? "9+" : unseenMessages[user._id]}
               </p>
             )}
           </div>
